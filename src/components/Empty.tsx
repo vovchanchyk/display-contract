6685e44c3cf12ff4f589bbd6bb460ed9b57a4b37
Vovchanchyk <oberigfast@gmail.com>
2021-05-19 21:15:59 +0300

    'loader'

--- src/components/Empty.tsx
+++ src/components/Empty.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import Loader from 'react-loader-spinner';
 
 const Empty = () => (
-  <div>
-    here is nothing...
+  <div className="app__loading">
+    <Loader
+      type="Puff"
+      color="#ffff"
+      height={100}
+      width={100}
+      // timeout={10000}
+    />
   </div>
 );
 export default Empty;
